879ab532bcd11344a9ea03b6d317f529fb4f3b6a
gustavoscruz <gustadscruz@gmail.com>
2023-10-18 19:49:35 -0300

    Creat and uploaded App.java

--- src/App.java
+++ src/App.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] elements = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int soma = SomaDeElementos.sum(elements);
-        System.out.println(soma);
+        int[] elements = {1, 2};
+        System.out.println(SomaDeElementos.sum(elements));
     }
 }
