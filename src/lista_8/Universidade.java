be5f8f8f16aa3eddf52820f2b599bb32879527aa
gustavoscruz <gustadscruz@gmail.com>
2023-11-14 17:37:19 -0300

    Updated Universidade Class

--- src/lista_8/Universidade.java
+++ src/lista_8/Universidade.java
@@ -1,4 +1,33 @@
 package lista_8;
 
+import java.util.ArrayList;
+
 public class Universidade {
+
+    private String nome;
+    private String endereco;
+    private ArrayList<Departamento> departamento = new ArrayList<Departamento>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void addDepartamento(Departamento departament){
+        departamento.add(departament);
+    }
+
+    public void removeDepartamento(Departamento departament){
+        departamento.remove(departament);
+    }
 }
