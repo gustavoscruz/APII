cbf3e6a03eb00935ae6ba4601ecb80c52d6e0036
gustavoscruz <gustadscruz@gmail.com>
2023-11-14 16:32:55 -0300

    created and modified lista_2 package

---
+++ src/lista_2/VogalConsoante.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class VogalConsoante {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        char letter;
+
+        System.out.print("Enter the letter: ");
+        letter = input.next().charAt(0);
+
+        switch(letter){
+            case 'a': case 'e': case 'i': case 'o': case 'u': {
+                System.out.print(letter + " is a vowel");
+                break;
+            }
+            default: {
+                System.out.print(letter + " is a consonant");
+                break;
+            }
+        }
+
+
+    input.close();
+    }
+}
