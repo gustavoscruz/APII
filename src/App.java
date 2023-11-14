cb2519726744876bec7442cb10d2128b1b0ea4b8
gustavoscruz <gustadscruz@gmail.com>
2023-11-14 16:18:22 -0300

    Created and update Arrays Class

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args){
        Scanner input = new Scanner(System.in);
 
        int[][] matrix = new int[3][3];
