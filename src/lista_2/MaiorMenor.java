b9db56166cab1df954f4393d05b7d3ca9f7be25a
gustavoscruz <gustadscruz@gmail.com>
2023-11-14 16:34:01 -0300

    modified lista_2 package

---
+++ src/lista_2/MaiorMenor.java
@@ -0,0 +1,23 @@
+package lista_2;
+
+import java.util.Scanner;
+public class MaiorMenor {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int number1, number2;
+
+        System.out.print("Enter the first number: ");
+        number1 = input.nextInt();
+        System.out.print("Enter the end number: ");
+        number2 = input.nextInt();
+
+        if(number1 > number2){
+            System.out.print("The number " + number1 + " is greater than the number " + number2);
+        }
+        else{
+            System.out.print("The number " + number2 + " is greater than the number " + number1);
+        }
+        input.close();
+    }
+}
