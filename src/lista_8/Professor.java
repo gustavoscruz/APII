54029f2e9debf91ffc8d6fb4024fe7498f58b48e
gustavoscruz <gustadscruz@gmail.com>
2023-11-14 16:58:51 -0300

    Updated Professor Class

--- src/lista_8/Professor.java
+++ src/lista_8/Professor.java
@@ -1,4 +1,41 @@
 package lista_8;
 
 public class Professor {
+    private String nome;
+    private String formacao;
+    private double salario;
+
+    public Professor(String nome, String formacao, double salario) {
+        this.nome = nome;
+        this.formacao = formacao;
+        this.salario = salario;
+    }
+
+    public Professor(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void reajustarSalario(double reajustePercentual){
+        this.salario = this.salario * (1 + (reajustePercentual / 100));
+    }
 }
