7f753b731282693619b77d71ee01d851185b83e9
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 07:57:10 -0300

    Updated Pais Class

--- src/lista_8/Pais.java
+++ src/lista_8/Pais.java
@@ -1,4 +1,31 @@
 package lista_8;
 
 public class Pais {
+    private String nome;
+    private double area;
+    private long populacao;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getArea(){
+        return area;
+    }
+
+    public void setArea(double area){
+        this.area = area;
+    }
+
+    public long getPopulacao(){
+        return populacao;
+    }
+
+    public void setPopulacao(long populacao){
+        this.populacao = populacao;
+    }
 }
