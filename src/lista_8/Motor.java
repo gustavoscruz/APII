db79e5d871ec0ce0f4872ed0eaacb7ff8d55f58d
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 10:27:26 -0300

    Update Motor Class

--- src/lista_8/Motor.java
+++ src/lista_8/Motor.java
@@ -1,4 +1,41 @@
 package lista_8;
 
 public class Motor {
+    private int potencia;
+    private double preco;
+    private String fabricante;
+
+    public Motor(int potencia, double preco, String fabricante) {
+        this.potencia = potencia;
+        this.preco = preco;
+        this.fabricante = fabricante;
+    }
+
+    public Motor(){
+
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
 }
