8a48e9d8483158d69e1a04a3e3fa25f10f646f12
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 10:30:23 -0300

    Update Bancos Class

--- src/lista_8/Bancos.java
+++ src/lista_8/Bancos.java
@@ -1,4 +1,41 @@
 package lista_8;
 
 public class Bancos {
+    private double preco;
+    private String tipo;
+    private String fabricante;
+
+    public Bancos(double preco, String tipo, String fabricante) {
+        this.preco = preco;
+        this.tipo = tipo;
+        this.fabricante = fabricante;
+    }
+
+    public Bancos(){
+
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
 }
