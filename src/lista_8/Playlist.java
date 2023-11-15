f4973b1bc2d56a5f610ec75d8c1d9893b09dbc73
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 07:08:18 -0300

    updated Playlist Class

--- src/lista_8/Playlist.java
+++ src/lista_8/Playlist.java
@@ -1,4 +1,61 @@
 package lista_8;
 
+import java.util.ArrayList;
+
 public class Playlist {
+    private String nome;
+    private ArrayList<Musica> musicas = new ArrayList<Musica>();
+
+    public Playlist(String nome){
+        this.nome = nome;
+    }
+    public Playlist(){
+
+    }
+
+    public String reproduzirMusica(Musica musica){
+        if(consultarMusica(musica.getIsrc()) == null) {
+            return "A musica " + musica.getTitulo() + " não está na playlista";
+        }
+        else {
+            return "A musica " + musica.getTitulo() + " está sendo reproduzida";
+        }
+    }
+
+    public Musica consultarMusica(String isrc){
+        for (Musica m : musicas){
+            if(m.getIsrc().equals(isrc)){
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public boolean adicionarMusica(Musica musica){
+        if(consultarMusica(musica.getIsrc()) == null){
+            musicas.add(musica);
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public boolean removerMusica(Musica musica){
+        if(consultarMusica(musica.getIsrc()) == null){
+            return false;
+        }
+        else {
+            musicas.remove(musica);
+            return true;
+        }
+    }
+
+    public String listarMusicas(){
+        for(Musica m : musicas){
+            return m.getIsrc();
+        }
+    }
+
 }
