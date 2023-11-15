37e7ee64f4f8d2967173a3c03e1ee76e181c5c0b
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 08:10:29 -0300

    created Continente Class

---
+++ src/lista_8/Continente.java
@@ -0,0 +1,24 @@
+package lista_8;
+
+import java.util.ArrayList;
+
+public class Continente {
+    private String nome;
+    private ArrayList<Pais> paises = new ArrayList<Pais>();
+
+    public Continente(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public boolean adicionarPais(Pais pais){
+
+    }
+
+    public Pais consultarPais(){
+
+    }
+
+}
