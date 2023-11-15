a6991eb1b4a09ff082b63edd1762359fc499e915
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 09:18:47 -0300

    Updated Continente Class

--- src/lista_8/Continente.java
+++ src/lista_8/Continente.java
@@ -14,11 +14,62 @@ public class Continente {
     }
 
     public boolean adicionarPais(Pais pais){
-
+        if(consultarPais(pais.getNome()) == null){
+            paises.add(pais);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    public Pais consultarPais(){
+    public Pais consultarPais(String nome){
+        for(Pais p : paises){
+            if(p.getNome().equals(nome)){
+                return p;
+            }
+        }
+        return null;
+    }
 
+    public double dimensaoDoContinente(){
+        double dimensao = 0;
+        for(Pais p : paises){
+            dimensao += p.getArea();
+        }
+        return dimensao;
+    }
+
+    public long populacaoDoContinente(){
+        long populacao = 0;
+        for(Pais p : paises){
+            populacao += p.getPopulacao();
+        }
+        return populacao;
+    }
+
+    public double densidadePopulacional(){
+        return populacaoDoContinente() / dimensaoDoContinente();
+    }
+
+    public Pais paisMaispopuloso(){
+        Pais maior = paises.get(0);
+        for(Pais p : paises){
+            if(maior.getPopulacao() > p.getPopulacao()){
+                maior = p;
+            }
+        }
+        return maior;
+    }
+
+    public Pais paisMenosPopuloso(){
+        Pais menor = paises.get(0);
+        for(Pais p : paises){
+            if(menor.getPopulacao() < p.getPopulacao()){
+                menor = p;
+            }
+        }
+        return menor;
     }
 
 }
