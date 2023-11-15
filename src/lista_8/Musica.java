8b0d4d880e61dd6bac8d406277224a026025bd09
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 06:46:16 -0300

    updated Musica Class

--- src/lista_8/Musica.java
+++ src/lista_8/Musica.java
@@ -1,4 +1,39 @@
 package lista_8;
 
 public class Musica {
+    private String titulo;
+    private String artista;
+    private int duracao;
+    private String isrc;
+
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getIsrc() {
+        return isrc;
+    }
+
+    public float controlarVelocidadeDeReproducao(float velocidade){
+        return this.duracao / velocidade;
+    }
 }
