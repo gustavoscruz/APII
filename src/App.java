5f38bb73d133ab6b4964c506522643f098eb370b
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 07:22:12 -0300

    updated App Class

--- src/App.java
+++ src/App.java
@@ -1,21 +1,18 @@
-import lista_7.Arrays;
 import lista_8.*;
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args){
 
-        Jogador gustavo = new Jogador("09197124524", "Gustavo", 100);
-        Jogador vinicius = new Jogador("09197124520", "Vinicius", 100);
+        Musica digaSimPraMim = new Musica("Diga sim pra mim", "desejo de menina", 3, "099");
+        Musica rapsodia = new Musica();
+        Playlist forro = new Playlist("Forro");
 
-        Time flamengo = new Time();
+        rapsodia.setIsrc("019");
 
-        flamengo.contrataJogador(gustavo);
-        flamengo.contrataJogador(vinicius);
-        System.out.println(flamengo.quantidadeJogadores());
-        flamengo.demitiJogador(vinicius);
-        System.out.println(flamengo.quantidadeJogadores());
-        System.out.println(flamengo.consultaJogador("09197124520"));
+        forro.adicionarMusica(digaSimPraMim);
+        forro.adicionarMusica(rapsodia);
+        System.out.println(forro.listarMusicas());
     }
 }
 
