693bfd08cb40aeb8cc11f513b2d7feb5d64f2991
gustavoscruz <gustadscruz@gmail.com>
2023-11-15 11:11:56 -0300

    Update App class

--- src/App.java
+++ src/App.java
@@ -3,27 +3,17 @@ import lista_8.*;
 
 public class App {
     public static void main(String[] args){
+        Pneus pirelli = new Pneus();
+        Motor mercedes = new Motor();
+        Bancos couro = new Bancos();
+        Pneus pipa = new Pneus(100, "Bom", "Gustavo");
+        Motor mercede = new Motor(200, 1000, "mercedes");
+        Bancos confort = new Bancos(5000, "bom", "national");
+        Carro fusca = new Carro(mercedes, pirelli, couro);
+        Carro camaro = new Carro(mercede, pipa, confort);
+        System.out.println(fusca.precoDoCarro());
+        System.out.println(camaro.precoDoCarro());
 
-        Pais brazil = new Pais("Brasil", 20000, 200000000);
-        Pais argentina = new Pais("Argentina", 10000, 30000000);
-        Pais equador = new Pais("Equador", 5000, 15000000);
-        Pais peru = new Pais("Peru", 1500, 200000);
-        Pais colombia = new Pais("colombia", 2000, 10000000);
-        Continente americaDoSul = new Continente("America do Sul");
-
-        americaDoSul.adicionarPais(brazil);
-        americaDoSul.adicionarPais(argentina);
-        americaDoSul.adicionarPais(equador);
-        americaDoSul.adicionarPais(peru);
-        System.out.println(americaDoSul.paisMaispopuloso());
-        System.out.println(americaDoSul.paisMenosPopuloso());
-        System.out.println(americaDoSul.densidadePopulacional());
-        System.out.println(americaDoSul.dimensaoDoContinente());
-        System.out.println(americaDoSul.populacaoDoContinente());
-        americaDoSul.adicionarPais(colombia);
-        System.out.println(americaDoSul.densidadePopulacional());
-        System.out.println(americaDoSul.dimensaoDoContinente());
-        System.out.println(americaDoSul.populacaoDoContinente());
     }
 }
 
